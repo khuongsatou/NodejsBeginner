a18478608e903c1267c9ac7b0c26934b0673dd05
Khuong <vankhuong240499@gmail.com>
2021-05-05 16:30:00 +0700

    app and detail

--- app.js
+++ app.js
@@ -1,30 +1,71 @@
-// const http = require("http");
-
 const hostname = "127.0.0.1";
 const port = 3000;
 const express = require("express");
 const app = express();
-//Config
-// const pug = require("pug");
+//Config PUG
 app.set("views", "./views"); // Thư mục views nằm cùng cấp với file app.js
 app.set("view engine", "pug"); // Sử dụng pug làm view engine
-// const compiledFunction = pug.compileFile("template.pug");
-// console.log(
-//   compiledFunction({
-//     name: "Timothy",
-//   })
-// );
+
+// Config express
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+var users = [
+  { id: 1, name: "User1", email: "user1@gmail.com", age: 31 },
+  { id: 2, name: "User2", email: "user2@gmail.com", age: 20 },
+  { id: 3, name: "User1", email: "user1.2@gmail.com", age: 25 },
+];
+
+app.get("/", function (req, res) {
+  res.send("<h2>This is my first app</h2>");
+});
 
 app.get("/hello-world", function (req, res) {
   res.send("Hello World");
 });
 
 app.get("/users", function (req, res) {
-  var users = [
-    { name: "User1", email: "user1@gmail.com" },
-    { name: "User2", email: "user2@gmail.com" },
-  ];
   res.render("users/index", { users: users });
+});
+
+app.get("/users/:id", (req, res) => {
+  // do some thing here
+  console.log(req.params);
+  // Tìm user phù hợp với params id
+  var user = users.find((user) => {
+    return user.id == parseInt(req.params.id);
+  });
+
+  // Render trang show, với một biến user được định nghĩa là user vừa tìm được
+  res.render("users/show", {
+    user: user,
+  });
+});
+
+app.get("/users/search", (req, res) => {
+  console.log(req.query);
+  var name_search = req.query.name;
+  var age_search = req.query.age;
+  var result = users.filter((user) => {
+    return (
+      user.name?.toLowerCase().indexOf(name_search?.toLowerCase()) !== -1 &&
+      user.age === parseInt(age_search)
+    );
+  });
+  res.render("users/index", {
+    users: result,
+  });
+});
+
+app.get("/users/create", (req, res) => {
+  res.render("users/create");
+});
+
+app.post("/users/create", (req, res) => {
+  // add new user here
+  console.log(req.body);
+  users.push(req.body);
+  res.redirect("/users");
 });
 
 app.get("/new-user", (req, res) => {
@@ -38,20 +79,10 @@ app.get("/new-user", (req, res) => {
     return res.sendStatus(400);
   }
 
-  // const salt = crypto.randomBytes(128).toString("base64");
-  // const hash = crypto.pbkdf2Sync(password, salt, 10000, 512, "sha512");
-
-  // users[username] = { salt, hash };
   res.send("username" + username + "- password" + password);
 
   res.sendStatus(200);
 });
-
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader("Content-Type", "text/plain");
-//   res.end("Hello World");
-// });
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
